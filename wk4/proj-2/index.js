f09cbaab2c838799abff6daaa86987ffa3674e26
bryan-huffstutler <bryan.huffstutler@bryanuniversity.edu>
2021-03-14 22:25:18 -0500

    proj-2-contacts

--- wk4/proj-2/index.js
+++ wk4/proj-2/index.js
@@ -4,50 +4,49 @@ const initialState = {
 }
 
 //Reducer
-function reducer (state = initialState, action) {
-  switch (action.type){
+function reducer(state = initialState, action) {
+  switch (action.type) {
     case 'ADD_NEW_PERSON':
-    var newArr = state.state.contacts
-    newArr.push(action.payload)
-      return {
-        state : {
-          contacts: newArr
-        }
-        
-    }
-
-    case 'REMOVE_PERSON': 
-      const arrCopy = state.state.contacts
-      let x = parseInt(action.payload)
-      const updatedArr = arrCopy.splice(x, 1)
-      console.log(state.state.contacts)
-      console.log("REMOVE_PERSON ACTION")
+      var newArr = state.state.contacts
+      newArr.push(action.payload)
       return {
         state: {
-          contacts: updatedArr
-        }  
+          contacts: newArr,
+          newContact: action.payload
+        }
       }
 
-    default: return {
-      state
-    }
-    
+      case 'REMOVE_PERSON':
+        const arrCopy = state.state.contacts
+        let x = parseInt(action.payload)
+        const updatedArr = arrCopy.filter(person => person.id !== x)
+        return {
+          state: {
+            contacts: updatedArr
+          }
+        }
+
+        default:
+          return {
+            state
+          }
+
   }
 }
 
 //Creating Store
 const store = Redux.createStore(reducer)
-let state = store.getState()
+
 
 //Actions
-function addNewPerson (person) {
+function addNewPerson(person) {
   return {
     type: 'ADD_NEW_PERSON',
     payload: person
   }
 }
 
-function removePerson (person) {
+function removePerson(person) {
   return {
     type: 'REMOVE_PERSON',
     payload: person
@@ -55,32 +54,40 @@ function removePerson (person) {
 }
 
 //Render function for subscribe
-function render () {
+function render() {
+  let state = store.getState()
   const contacts = document.getElementById('contacts')
-  contacts.innerHTML=""
+  let x = state.state.newContact
   console.log("in the render", state.state.contacts)
-  state.state.contacts.map((x, i) => {
-    let div = document.createElement('div')
-    let h3 = document.createElement('h3')
-    let h4email = document.createElement('h4')
-    let h4phone = document.createElement('h4')
-    let btn = document.createElement('button')
-    btn.textContent = 'Delete'
-    btn.classList.add('del')
-    h4phone.innerHTML = `Phone: ${x.phone}`
-    h4email.innerHTML = `Email: ${x.email}`
-    h3.innerHTML = `Name: ${x.firstName} ${x.lastName}`
-    div.id=i
-    div.appendChild(h3)
-    div.appendChild(h4email)
-    div.appendChild(h4phone)
-    div.appendChild(btn)
-    contacts.appendChild(div)
+  let div = document.createElement('div')
+  let h3 = document.createElement('h3')
+  let h4email = document.createElement('h4')
+  let h4phone = document.createElement('h4')
+  let btn = document.createElement('button')
+  btn.textContent = 'Delete'
+  btn.classList.add('del')
+  h4phone.innerHTML = `Phone: ${x.phone}`
+  h4email.innerHTML = `Email: ${x.email}`
+  h3.innerHTML = `Name: ${x.firstName} ${x.lastName}`
+  div.id = state.state.contacts.length - 1
+  btn.id = state.state.contacts.length - 1
+  btn.addEventListener('click', function (e) {
+    let x = btn.parentNode
+    console.log(x)
+    if (e.target.id == btn.id) {
+      contacts.removeChild(x)
+      store.dispatch(removePerson(x.id))
+    }
   })
+  div.appendChild(h3)
+  div.appendChild(h4email)
+  div.appendChild(h4phone)
+  div.appendChild(btn)
+  contacts.appendChild(div)
 }
 
 //Subscribe
-store.subscribe(render)
+store.subscribe(() => console.log(store.getState()))
 
 //Event Listeners
 document.getElementById('sub-add')
@@ -90,18 +97,12 @@ document.getElementById('sub-add')
     const lastName = document.getElementById('add-last-name').value
     const email = document.getElementById('add-email').value
     const phone = document.getElementById('add-phone').value
-    store.dispatch(addNewPerson({firstName: firstName, lastName: lastName, email: email, phone: phone, id: store.getState().state.contacts.length}))
+    store.dispatch(addNewPerson({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      phone: phone,
+      id: store.getState().state.contacts.length
+    }))
+    render()
   })
-
-//Deleting from DOM
-let deleteBtn = document.getElementsByClassName('del')
-contacts.addEventListener('click', function(e){  
-  for (i=0; i<deleteBtn.length; i++){
-    if (e.target == deleteBtn[i]){
-      let x = deleteBtn[i].parentNode
-      contacts.removeChild(x)
-    }
-  }
-  let x = e.target.parentNode.id
-  store.dispatch(removePerson(x))
-})
